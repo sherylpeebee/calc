19acf3de90609f76a4ed688bfb0e1b56ade2932c
Sheryl Boughton <sherpbo@gmail.com>
2015-08-29 22:22:46 -0700

    ok

--- js/main.js
+++ js/main.js
@@ -1,56 +1,62 @@
-$(document).ready(init);
+$(document).ready(function(){
+  $('.number').click(clickNumber);
+  $('#decimal').click(clickDecimal);
+  $('#all-clear').click(clear);
+  $('.operator').click(storeData);
+  $('#equal').click(crunchNumbers);
+});
 
-function init(){
-  $(".numbers").on("click", numbersClicked);
-  $(".operator").on("click", operatorClicked);
-  $('#decimal').on("click", decimalClicked);
-}
+var x;
+var operator;
+var output;
 
-var numArr = [];
-// var opArray = []
-function numbersClicked(){
-  var initialNum = parseFloat($(this).attr("value"));
-  numArr.push(initialNum);
-  var appendedNum = parseFloat(numArr.join(''));
-  // console.log(appendedNum);
-  if(numArr[0] && numArr.indexOf("+") === -1 && numArr.indexOf("-") === -1 && numArr.indexOf("/") === -1 && numArr.indexOf("*") === -1){
-    numArr = [];
-    numArr.push(appendedNum);
-    console.log(numArr);
-  }
-  else {
-    var firstPartOfOperation = numArr.splice(0, 2);
-    console.log("firstPartOfOperation: ", firstPartOfOperation);
-    // console.log("secondNum: ", numArr[2]);
-  }
-  $("#input").val(numArr);
-}
-
-function operatorClicked(){
-  var op = $(this).attr("value");
-  numArr.push(op);
-  console.log("numArr: ", numArr);
-}
-
-function decimalClicked(){
-  var input = $('#input').val();
-  var output = input.indexOf('.') !== -1 ? input : input += '.';
+function clickNumber(){
+  var num = $(this).text();
+  var display = $('#input').val();
+  output = (display === '0') ? num : display + num;
   $('#input').val(output);
 }
 
-// switch (op) {
-//   case "+":
-//   $("#input").val('');
-//
-//     break;
-//   case "-":
-//
-//     break;
-//   case "/":
-//
-//     break;
-//   case "*":
-//
-//     break;
-//   default:
-// }
+function clickDecimal(){
+  var display = $('#input').val();
+  var output = display.indexOf('.') !== -1 ? display : display += '.';
+  $('#input').val(output);
+}
+
+function clear(){
+  $('#input').val('0');
+}
+
+function storeData(){
+  operator = $(this).attr("value");
+  console.log(operator);
+  var lastNum = $('#input').val();
+  x = parseFloat($('#input').val());
+  clear();
+}
+
+function crunchNumbers(){
+  console.log('click');
+  var y = parseFloat($('#input').val());
+  console.log(x, y);
+  var result;
+  switch (operator){
+    case '+':
+    console.log('operator');
+      result = x + y;
+      console.log(result);
+      break;
+    case '-':
+      result = x - y;
+      console.log(result);
+      break;
+    case '*':
+      result = x * y;
+      console.log(result);
+      break;
+    case '/':
+      result = x / y;
+      console.log(result);
+  }
+  $('#input').val(result);
+}
